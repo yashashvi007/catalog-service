f7250ae1d253a57dee242a224d8b7e9513cd2274
yashashvi007 <yashashvimaurya@gmail.com>
2026-01-05 20:02:18 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import logger from "./config/logger";
+import config from "config";
+import { initDb } from "./config/db";
+
+const startServer = async () => {
+    const PORT: number = config.get("server.port") || 5502;
+    try {
+        await initDb();
+        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            logger.on("finish", () => {
+                process.exit(1);
+            });
+        }
+    }
+};
+
+void startServer();
