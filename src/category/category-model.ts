8b2e68524975518271d8a4ab6c8b6e41a0637179
yashashvi007 <yashashvimaurya@gmail.com>
2026-01-05 22:50:08 +0530

    Add category model with Mongoose schemas for price configuration and attributes

---
+++ src/category/category-model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+export interface PriceConfiguration {
+    [key: string]: {
+        priceType: 'base' | 'additional';
+        availableOptions: string[];
+    }
+}
+
+export interface Attribute {
+    name: string;
+    widgetType: 'switch' | 'radio';
+    defaultValue: string;
+    availableOptions: string[];
+}
+
+export interface Category {
+    name: string;
+    priceConfiguration: PriceConfiguration;
+    attributes: Attribute[];
+}
+
+const attributeSchema = new mongoose.Schema<Attribute>({
+    name: {type: String, required: true},
+    widgetType: {type: String, enum: ["switch", "radio"], required: true},
+    defaultValue: {type: mongoose.Schema.Types.Mixed, required: true},
+    availableOptions: {type: [String], required: true}
+})
+
+const priceConfigurationSchema = new mongoose.Schema<PriceConfiguration>({
+    priceType: {
+        type: String, 
+        enum: ["base", "additional"],
+        required: true
+    },
+    availableOptions: {
+        type: [String],
+        required: true
+    }
+})
+
+const categorySchema = new mongoose.Schema<Category>({
+    name: {type: String, required: true},
+    priceConfiguration: {type: Map,of: priceConfigurationSchema, required: true},
+    attributes: {type: [attributeSchema], required: true}
+})
+
+export default mongoose.model("Category", categorySchema);
